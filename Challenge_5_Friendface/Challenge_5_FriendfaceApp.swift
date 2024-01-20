40b9abe3c77b7b4a8e7f36693fd9f62f557ab1d7
Daniel Freire <dgz1606@gmail.com>
2024-01-20 18:42:48 -0500

    Initial Commit

---
+++ Challenge_5_Friendface/Challenge_5_FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_5_FriendfaceApp.swift
+//  Challenge_5_Friendface
+//
+//  Created by Daniel Freire on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_5_FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
